ab7bebc1ea6d8233349681d579f9071f1186cf24
Simon Conrad <simondbconrad@gmail.com>
2022-12-13 11:03:51 -0600

    write playRound if/else

--- script.js
+++ script.js
@@ -18,17 +18,44 @@ const getPlayerChoice = function() {
     } else {getPlayerChoice();};
 }
 
+// getPlayerChoice()
+// console.log(playerChoice);
+
 const playRound = function(playerChoice, computerChoice){
     playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
     console.log(playerChoice);
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    if (playerChoice === 0 && computerChoice === 1) {
+        result = 'Computer wins! Paper covers Rock!' ;
+        return result
+    } else if (playerChoice === 0 && computerChoice === 2){
+        result = 'You win! Rock smashes Scissors!' ;
+        return result;
+    } else if (playerChoice === 1 && computerChoice === 0){
+        result = 'You win! Paper covers Rock!' ;
+        return result;
+    }  else if (playerChoice === 1 && computerChoice === 2){
+        result = 'You lose! Scissors cut Paper!' ; 
+        return result;// win/lose outcomes for Paper
+    } else if (playerChoice === 2 && computerChoice === 0){
+        result = 'You lose! Rock smashes Scissors!' ;
+        return result;
+    } else if (playerChoice === 2 && computerChoice === 1){
+        result = 'You win! Scissors cut paper!' ;
+        return result ; //win/lose outcomes for Scissors
+    } else {
+        alert ('Draw!') ;
+        playRound() ;
+    } //draw outcome   
+    console.log(result);
 }
+
 
 playRound();
 
 
-// getPlayerChoice();
-// console.log(playerChoice);
+
 
 
 
