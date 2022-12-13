8ff8734a4255390e9e753c0a7b68cfb6af6bc041
Simon Conrad <simondbconrad@gmail.com>
2022-12-13 09:32:07 -0600

    clean up getComputerChoice - change output to computerChoice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const getComputerChoice = function(num){
-    num = Math.floor(Math.random()*3);
-    return num;
+const getComputerChoice = function(){
+    computerChoice = Math.floor(Math.random()*3);
+    return computerChoice;
 }
-console.log(getComputerChoice(0));
+
