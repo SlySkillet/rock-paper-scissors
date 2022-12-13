73d58af82b44ab366ceebf4b66b954237d9c0a82
Simon Conrad <simondbconrad@gmail.com>
2022-12-13 10:16:48 -0600

    create playRound function - problem! in the case of a typo, prompt player to try again

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const getComputerChoice = function(){
 const playRound = function(playerChoice, computerChoice){
     playerChoice = prompt ('Rock, Paper or Scissors?');
     playerChoice = playerChoice.toUpperCase();
+    if (playerChoice === 'ROCK') {
+        playerChoice = 0 ;
+    } else if (playerChoice === 'PAPER'){
+        playerChoice = 1 ;
+    } else if (playerChoice === 'SCISSORS') {
+        playerChoice = 2 ;
+    } else {playRound();};
+    console.log(playerChoice);
 }
 
 playRound();
