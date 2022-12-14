08843884c77ef2b0664b6bbd5b286025d9636fcc
Simon Conrad <simondbconrad@gmail.com>
2022-12-14 09:28:02 -0600

    change numbers to 'rock', 'paper' and 'scissors'

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
 const getComputerChoice = function(){
     computerChoice = Math.floor(Math.random()*3);
-    return computerChoice;
+    if (computerChoice === 0) {
+        computerChoice = 'ROCK' ;
+        return computerChoice;
+    } else if (computerChoice === 1) {
+        computerChoice = 'PAPER'
+        return computerChoice;
+    } else {computerChoice = 'SCISSORS'
+        return computerChoice;
+    }
 }
 
 const getPlayerChoice = function() {
     playerChoice = prompt ('Rock, Paper or Scissors?');
     playerChoice = playerChoice.toUpperCase();
-    if (playerChoice === 'ROCK') {
-        playerChoice = 0 ;
-        return playerChoice;
-    } else if (playerChoice === 'PAPER'){
-        playerChoice = 1 ;
-        return playerChoice;
-    } else if (playerChoice === 'SCISSORS') {
-        playerChoice = 2 ;
-        return playerChoice;
-    } else {getPlayerChoice();};
+    return playerChoice;
 }
 
-// getPlayerChoice()
-// console.log(playerChoice);
+// 
+
 
 const playRound = function(playerChoice, computerChoice){
     playerChoice = getPlayerChoice();
     console.log(playerChoice);
     computerChoice = getComputerChoice();
     console.log(computerChoice);
-    if (playerChoice === 0 && computerChoice === 1) {
+    if (playerChoice === 'ROCK' && computerChoice === 'PAPER') {
         result = 'Lose' ;
         console.log('You lose! Paper covers Rock!');
         return result
-    } else if (playerChoice === 0 && computerChoice === 2){
+    } else if (playerChoice === 'ROCK' && computerChoice === 'SCISSORS'){
         result = 'Win' ;
         console.log('You win! Rock smashes Scissors!');
         return result;
-    } else if (playerChoice === 1 && computerChoice === 0){
+    } else if (playerChoice === 'PAPER' && computerChoice === 'ROCK'){
         result = 'Win' ;
         console.log('You win! Paper covers Rock!');
         return result;
-    }  else if (playerChoice === 1 && computerChoice === 2){
+    }  else if (playerChoice === 'PAPER' && computerChoice === 'SCISSORS'){
         result = 'Lose' ; 
         console.log('You lose! Scissors cut paper!');
         return result;// win/lose outcomes for Paper
-    } else if (playerChoice === 2 && computerChoice === 0){
+    } else if (playerChoice === 'SCISSORS' && computerChoice === 'ROCK'){
         result = 'Lose' ;
         console.log('You lose! Rock smashes Scissors!');
         return result;
-    } else if (playerChoice === 2 && computerChoice === 1){
+    } else if (playerChoice === 'SCISSORS' && computerChoice === 'PAPER'){
         result = 'Win' ;
         console.log('You win! Scissors cut paper!');
         return result ; //win/lose outcomes for Scissors
@@ -57,28 +56,45 @@ const playRound = function(playerChoice, computerChoice){
    
 }
 
-//GAME OF 5//
-let playerWin = 0
-let computerWin = 0
-const game = function() {
-    for (let i = 0 ; i < 5 ; i ++) {
-        playRound() ;
-        if (result === 'Win') {
-            playerWin += 1 ;
-        } else {computerWin += 1}
-        console.log(playerWin, computerWin);
-        if (playerWin === 3) {
-            console.log (`You win! ${playerWin} to ${computerWin}!`);
-            break;
-        } else if (computerWin === 3) {
-            console.log (`You lose! ${computerWin} to ${playerWin}`);
-            break;
-        }
-    }
-}
+// //GAME OF 5//
+// let playerWin = 0
+// let computerWin = 0
+// const game = function() {
+//     for (let i = 0 ; i < 5 ; i ++) {
+//         playRound() ;
+//         if (result === 'Win') {
+//             playerWin += 1 ;
+//         } else {computerWin += 1}
+//         console.log(playerWin, computerWin);
+//         if (playerWin === 3) {
+//             console.log (`You win! ${playerWin} to ${computerWin}!`);
+//             break;
+//         } else if (computerWin === 3) {
+//             console.log (`You lose! ${computerWin} to ${playerWin}`);
+//             break;
+//         }
+//     }
+// }
 
 
-game()
+// game()
+
+//ORIGINAL GETPLAYERCHOICE FUNCTION//
+
+// const getPlayerChoice = function() {
+//     playerChoice = prompt ('Rock, Paper or Scissors?');
+//     playerChoice = playerChoice.toUpperCase();
+//     if (playerChoice === 'ROCK') {
+//         playerChoice = 0 ;
+//         return playerChoice;
+//     } else if (playerChoice === 'PAPER'){
+//         playerChoice = 1 ;
+//         return playerChoice;
+//     } else if (playerChoice === 'SCISSORS') {
+//         playerChoice = 2 ;
+//         return playerChoice;
+//     } else {getPlayerChoice();};
+// }
 
 //ORIGINAL PLAYROUND FUNCTION//
 
